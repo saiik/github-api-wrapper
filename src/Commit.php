574cbb96366595637a8ecd4c0403dfcc5f89acd0
Tobias Fuchs <tobiasf@voll.digital>
2016-10-19 14:05:58 +0200

    comments

--- src/Commit.php
+++ src/Commit.php
@@ -1,20 +1,46 @@
 <?php
 
+declare(strict_types=1);
+
 namespace saiik;
 
+/**
+ * @package saiik\
+ * @author Tobias Fuchs <saikon@hotmail.de>
+ * @version 1.0
+ */
 class Commit {
 
+	/**
+	 * @var string $sha
+	 *
+	 */
 	protected $sha;
+
+	/**
+	 * @var \stdClass $commit
+	 *
+	 */
 	protected $commit;
+
+	/**
+	 * @var string $rOWner
+	 *
+	 */
 	protected $rOwner;
+
+	/**
+	 * @var string $rName
+	 *
+	 */
 	protected $rName;
 
 	public function __construct(\stdClass $commit, Repository $repo) {
-
 		$this->sha = $commit->sha;
 		$this->commit = $commit->commit;
 		$this->rOwner = $repo->owner;
 		$this->rName = $repo->name;
+
 	}
 
 	public function __get($name) {
